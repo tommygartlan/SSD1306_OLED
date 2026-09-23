fe45c076b90d024fbe94dc9797966e1b5303a18b
Thomas(Tommy) Gartlan <tgartlan@gmail.com>
2026-09-23 16:23:15 +0100

    Update ssd1306_v2.c
    Added stdint and stdbool header files. Placed the custom header files for i2c and ssd after these

--- ssd1306_v2.c
+++ ssd1306_v2.c
@@ -1,10 +1,13 @@
-#include "ssd1306_v2.h"
 //#include "../Proj_26.X/mcc_generated_files/system/system.h"
 #include <xc.h>
 //#include"../S_26.X/mcc_generated_files/system/clock.h"   //Clock freq from project setting
 #include "clock.h" //in project properties , global compiler options, include directories (mcc_generated_files/system)
-#include "../Curioisty_I2C_Library-main/I2C_Func_Simple_v2.h"
 #include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "../Curioisty_I2C_Library-main/I2C_Func_Simple_v2.h"
+#include "ssd1306_v2.h"
 
 #define SSD1306_I2C_ADDR   0x3D
 
